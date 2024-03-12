a50966d27706ef6d4eb6dad951a0970b85c7864f
Luhz <1393454295@qq.com>
2024-03-12 10:55:13 +0800

    Add trace plugin

---
+++ Demo1/Plugins/Trace/Source/Trace/Private/SceneUIActor.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SceneUIActor.h"
+
+// Sets default values
+ASceneUIActor::ASceneUIActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	UI = CreateDefaultSubobject<UWidgetComponent>(TEXT("UI"));
+	UI->SetWidgetSpace(EWidgetSpace::Screen);
+	UI->SetWidgetClass(UIClass);
+}
+
+void ASceneUIActor::SetVisible(bool NewVisible, bool bForce)
+{
+	if (bForce)
+	{
+		bVisible = NewVisible;
+		OnSelected(bVisible);
+	}
+	else
+	{
+		if (NewVisible != bVisible)
+		{
+			bVisible = NewVisible;
+			OnSelected(bVisible);
+		}
+	}
+}
+
+void ASceneUIActor::SetSelected(bool bSelected, bool bDispatchSelectedEvent)
+{
+	if (bIsSelected != bSelected)
+	{
+		bIsSelected = bSelected;
+		OnSelected(bSelected);
+	}
+}
+
+// Called when the game starts or when spawned
+void ASceneUIActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ASceneUIActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
